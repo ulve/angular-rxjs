d5a1cbed2ad9d05032d492513e27c34733a7e91c
Olov Johansson <johansson.olov@gmail.com>
2016-11-13 19:00:36 +0100

    Lagt till funktioner för att hämta delkrav och exponera dom så komponenter lätt kan prenumerera på dom

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,33 @@
+import { Observable } from 'rxjs/Rx';
+import { Http } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Injectable } from '@angular/core';
+import { Delkrav } from './delkrav';
 
 @Injectable()
 export class DataService {
+  delkrav: Observable<Delkrav[]>
+  private _delkrav: BehaviorSubject<Delkrav[]>;
+  private baseUrl: string;
+  private delkravStore: {
+    delkrav: Delkrav[]
+  }
 
-  constructor() { }
+  constructor(private http: Http) { 
+    this.baseUrl = 'http://5820290b8b129a110026d7c6.mockapi.io/api/v1';
+    this.delkravStore = {delkrav: []};
+    this._delkrav = <BehaviorSubject<Delkrav[]>> new BehaviorSubject([]);
+    this.delkrav = this._delkrav.asObservable();
+  }
 
+  loadAllDelkrav() {
+    console.log('Anropar webbservice');
+    this.http.get(`${this.baseUrl}/delkrav`)
+      .map(response => response.json())
+      .subscribe(data => {
+        this.delkravStore.delkrav = data;
+        this._delkrav.next(Object.assign({}, this.delkravStore).delkrav);
+      }, error => console.log('Kunde inte ladda delkrav.'), 
+         ()    => console.log('Klar!'));     
+  }
 }
---
+++ src/app/delkrav.ts
@@ -0,0 +1,5 @@
+export interface Delkrav {
+    id: Number;
+    delkravstyp: String;
+    belopp: Number;
+}
